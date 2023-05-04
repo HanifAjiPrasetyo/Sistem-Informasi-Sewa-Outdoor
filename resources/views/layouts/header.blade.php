3250865f4f6ccf1c057c0555416405d7b6e84218
HanifAjiPrasetyo <hanifaji007@gmail.com>
2023-05-04 09:24:01 +0700

    set template and layouting #2

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,19 @@
+<header class="bg-gradient-dark">
+    <div class="page-header min-vh-75" style="background-image: url('../assets/img/bg9.jpg');">
+        <span class="mask bg-gradient-dark opacity-6"></span>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8 text-center mx-auto my-auto mt-4">
+                    <h2 class="text-white">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi, dolorem.
+                    </h2>
+                    <p class="lead mb-4 text-white opacity-8">Lorem ipsum dolor sit amet consectetur adipisicing
+                        elit. A ut omnis quaerat animi doloremque corporis vero totam autem iusto error, eligendi
+                        illo magnam sit sapiente! Illo aliquid nihil odit dicta.</p>
+                    <button type="submit" class="btn bg-white text-dark">Lihat Selengkapnya <span
+                            class="material-icons">arrow_forward</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,1 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" itemscope itemtype="http://schema.org/WebPage">
 
+<head>
+    @include('layouts.head-content')
+</head>
+
+<body class="bg-gray-200">
+
+    @include('layouts.header')
+
+    @include('layouts.navbar')
+
+    <div class="card card-body shadow-xl mx-3 mx-md-4 mt-n6">
+
+        <section class="py-7">
+            @yield('container')
+        </section>
+
+    </div>
+
+    @include('layouts.footer')
+
+    @include('layouts.foot-content')
+
+</body>
+
+</html>

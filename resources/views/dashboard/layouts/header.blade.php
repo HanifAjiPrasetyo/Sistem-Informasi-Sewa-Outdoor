8b35b53449c7b45f7706a9fc48908f32284ea698
indradprasetya <indrayanadian7@gmail.com>
2023-05-11 21:39:18 +0700

    edit header

--- resources/views/dashboard/layouts/header.blade.php
+++ resources/views/dashboard/layouts/header.blade.php
@@ -3,7 +3,7 @@
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
                 <li class="breadcrumb-item text-sm">
-                    <a class="opacity-5 text-dark" href="javascript:;">Pages</a>
+                    <a class="opacity-5 text-dark" href="/dashboard">Pages</a>
                 </li>
                     @yield('menu-optional')
                 <li class="breadcrumb-item text-sm text-dark active" aria-current="page">
